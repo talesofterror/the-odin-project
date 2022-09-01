c02749519aea2d29c303a0f6e330ef468e28c574
mike <mich.rivera84@gmail.com>
2022-08-31 23:23:46 -0400

    sandbox2 for javascript-notes.txt

---
+++ sandbox/sandbox1/script.js
@@ -0,0 +1,30 @@
+
+// let number1 = 1
+
+// console.log(number1 && 3)
+
+let a = prompt("First Number?", 1)
+let b = prompt("Second Number?", 2)
+
+console.log(+a + +b)
+
+
+/*
+
+1 = 0
+2 = -1
+3 = 1  ~ I'm assuming true evaluates to 1 and false evaluates to 0
+4 = 2
+5 = 6
+6 = NaN
+7 = NaN  ~ the expression is ["$" + 4 + 5]
+8 = 2
+9 = NaN
+10 = [ -9 5]
+11 = NaN  ~ expression is [" -9 " -5]
+12 = 1  ~ the expression is [null + 1]
+13 = NaN
+14 = NaN
+
+*/
+
