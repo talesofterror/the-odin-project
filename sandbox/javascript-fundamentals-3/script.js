6f9f8a81d60c1c28f5689bd2a98b3c64a7f3ec8d
mike <mich.rivera84@gmail.com>
2022-09-05 18:29:57 -0400

    nearly for the function menu working

--- sandbox/javascript-fundamentals-3/script.js
+++ sandbox/javascript-fundamentals-3/script.js
@@ -1,5 +1,35 @@
 const paraAdd7 = document.createElement('p');
-const input = document.querySelectorAll(".input")[0]
+
+
+
+const idAdd7 = document.getElementById('buttonAdd7')
+const idMultiply = document.getElementById('buttonMultiply')
+const idCapitalize = document.getElementById('buttonCapitalize')
+const ifLastLetter = document.getElementById('buttonLastLetter')
+
+let activeElement = document.querySelector("#content")
+
+function buttonClick() {
+    const buttonAdd7 = document.getElementById('buttonAdd7')
+    const buttonMultiply = document.getElementById('buttonMultiply')
+    const buttonCapitalize = document.getElementById('buttonCapitalize')
+    const buttonLastLetter = document.getElementById('buttonLastLetter')
+    
+    // let targetElement = document.getElementById(id1).classList.remove("hidden")
+
+    // for (i = 0; i <=activeElement.children.length; i++) {
+    //     if (activeElement.id === id1){ return }
+    //     else { activeElement.children[i].classList.add("hidden") }
+    // }
+
+    console.log(this.id)
+}
+
+const inputAdd7 = document.getElementById('inputAdd7')
+const inputMultiply = document.getElementById('inputAdd7')
+const inputCapitlize = document.getElementById('inputAdd7')
+const inputLastLetter = document.getElementById('inputAdd7')
+
 
 paraAdd7.textContent = "Type in a number and press Enter"
 
@@ -7,8 +37,8 @@ function add7(n) {
     if (isNaN(n) || n === '') {
         return "Not a number"
     } else {
-    result = Number(n) + 7
-    return result 
+        result = Number(n) + 7
+        return result
     }
 }
 
@@ -22,25 +52,26 @@ function capitalize(input) {
     return result
 }
 
-function lastLetter (input) {
+function lastLetter(input) {
     stringify = input.toString()
     result = stringify.charAt(stringify.length - 1)
     return result
 }
 
-function functionAdd7() {
-    console.log("Hello.")
-}
+/*
+If the element is currently active
+then add class "hidden"
+and remove hidden from target ID. 
+*/
 
 // let promptTest = prompt("Enter a Number")
 // console.log(promptTest)
 
 
 
-input.addEventListener("change", 
+inputAdd7.addEventListener("change",
     () => paraAdd7.textContent = `Result: ${add7(input.value)}`)
 
 const section = document.querySelector('section');
 
 section.appendChild(paraAdd7);
-
