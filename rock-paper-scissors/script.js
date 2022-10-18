3fc47804c317a1ca046b81510fa91ca56bf72902
mike <mich.rivera84@gmail.com>
2022-10-17 23:16:20 -0400

    rps

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,16 +1,25 @@
+let input = document.querySelector(".userInput").value;
+let result = document.querySelector(".result") 
+result.textContent = input
+
 let choice = ["rock", "paper", "scissors"]
 
-let playerChoice = "playerChoice"
-let computerChoice = "computerChoice"
+let playerChoice = input
+let computerChoice
 
 let fighter = {
     player : playerChoice,
     computer : computerChoice
 }
 
+
 let randomValue = () => Math.floor(Math.random() * 3)
+
+
+computerChoice = choice[randomValue()]
+
+
 
 // console.log(window.crypto.getRandomValues())
 
-let val3 = 0xf000001f, val4 = 1
-console.log(Math.imul(val3, val4))
+
