29c30f1e4f498891e54e54a7379844d1e6db98d3
mike <mich.rivera84@gmail.com>
2022-10-25 22:38:59 -0400

    made a bunch of changes in the design, including colors and fonts. javascript: got rid of that useless object, refactored a bit

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,24 +1,22 @@
 let userChoice = document.querySelector(".input")
 let prompt = document.querySelector(".prompt") 
-prompt.textContent = "Choose a fighter"
+prompt.textContent = "Choose a fighter:"
+let fightButton = document.querySelector(".fight-button")
 
 let choice = ["rock", "paper", "scissors"]
 
-let playerChoice = userChoice
 let computerChoice
 let randomValue = () => Math.floor(Math.random() * 3)
 
-let fighter = {
-    player : playerChoice,
-    computer : computerChoice
-}
-
-
-
 addEventListener ("change", 
 () => {
+	prompt.textContent = choiceDeclarer()	
 
-	prompt.textContent = choiceDeclarer()
+	if (userChoice.value == "default") {
+		fightButton.style.display = "none"
+	} else {
+		fightButton.style.display = "block"
+	}
 
 })
 
