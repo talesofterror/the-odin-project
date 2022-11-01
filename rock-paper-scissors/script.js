3d0c59b480063283ce45dcfc728dfbbf6c54b561
mike <mich.rivera84@gmail.com>
2022-10-31 23:45:29 -0400

    made a function for a single game as well as the rounds. Now I need to find a way to tally the number of wins and losses and print a final decision at the end. Separated the functions with comments into "game functions" and "design functions".

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -3,10 +3,12 @@ let prompt = document.querySelector(".prompt")
 prompt.textContent = "Choose a fighter:"
 let fightButton = document.querySelector(".fight-button")
 
-let choice = ["rock", "paper", "scissors"]
+// let choice = ["rock", "paper", "scissors"]
 
-let computerChoice
 let randomValue = () => Math.floor(Math.random() * 3)
+let computerChooser = () => choice[randomValue()]
+let c
+let numberOfRounds = 5
 
 addEventListener ("change", 
 () => {
@@ -16,9 +18,43 @@ addEventListener ("change",
 		fightButton.style.display = "none"
 	} else {
 		fightButton.style.display = "block"
+		game()
 	}
 
 })
+
+// GAME FUNCTIONS
+
+function game() {
+	let result
+	c = computerChooser()
+	if (userChoice.value == c) {
+		result = "Tie!"
+	} else if (userChoice.value == "rock" && c == "paper") {
+		result = "You lose"
+	} else if (userChoice.value == "rock" && c == "scissors") {
+		result = "You win!"
+	} else if (userChoice.value == "paper" && c == "rock") {
+		result = "You win!" 
+	} else if (userChoice.value == "paper" && c == "scissors") {
+		result = "You lose!"
+	} else if (userChoice.value == "scissors" && c == "rock") {
+		result = "You lose!" 
+	} else if (userChoice.value == "scissors" && c == "paper") {
+		result = "You win!"
+	}
+
+	return result
+}
+
+function rounds () {
+	let result = []
+	for (i = 0; i < numberOfRounds; i ++) {
+		console.log("Round " + (i+ 1) + ": " + game())
+	}
+}
+
+// DESIGN FUNCTIONS
 
 function choiceDeclarer() {
 	let promptText;
@@ -29,12 +65,5 @@ function choiceDeclarer() {
 	}
 	return promptText
 }
-
-
-computerChoice = choice[randomValue()]
-
-
-
-// console.log(window.crypto.getRandomValues())
 
 
