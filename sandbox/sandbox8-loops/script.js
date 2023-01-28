c9bb0877c0a148fc5fe7e39d6d8fb0a0b9eaf046
mike <mich.rivera84@gmail.com>
2023-01-27 23:56:13 -0500

    sandbox8-loops

--- sandbox/sandbox8-loops/script.js
+++ sandbox/sandbox8-loops/script.js
@@ -6,10 +6,9 @@ let pets = ["Melvin", "Miles", "Tyson", "Kitty", "Brownie", "Zeus", "Harley"]
 
 // for..of loop
 
-let title = document.createElement("span")
-title.textContent = "for..of loop: Pets[]"
-document.querySelector(".container").appendChild(title)
-document.querySelector(".container").insertBefore(title, getId("pets"))
+let forOfTitle = document.createElement("span")
+forOfTitle.textContent = "for..of loop: Pets[]"
+document.querySelector(".container").insertBefore(forOfTitle, getId("pets"))
 
 for (pet of pets) {												// logs the value of each index
 	getId("pets").textContent += pet + " "
@@ -21,8 +20,26 @@ for (pet in pets) {												// logs index numbers, no values
 
 // map
 
-let setToJake = (string) => string = "jake"
+let setToJake = (string) => string = "jake "
 
 let petsMapped = pets.map(setToJake)
 
-console.log(petsMapped)
+let mappedSection = document.createElement("section")
+mappedSection.id = "mapped"
+mappedSection.textContent = petsMapped
+document.querySelector(".container").appendChild(mappedSection)
+
+let mappedTitle = document.createElement("span")
+mappedTitle.textContent = "Pets[] mapped"
+document.querySelector(".container").insertBefore(mappedTitle, getId("mapped"))
+
+// filter 
+
+function firstLetter (string) { 
+	if (string.charAt(0) == "M"){	// could also use string.startsWith("M")
+		return string
+	}
+}
+
+let petsFiltered = pets.filter(firstLetter)
+console.log(petsFiltered)
