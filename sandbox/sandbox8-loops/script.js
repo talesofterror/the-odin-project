17e97322cc160475f77974c648289bab39fb23ea
mike <mich.rivera84@gmail.com>
2023-02-07 22:09:59 -0500

    sandbox8 - recreated one of the lesson examples without looking; made all the html elements in js.

--- sandbox/sandbox8-loops/script.js
+++ sandbox/sandbox8-loops/script.js
@@ -52,4 +52,40 @@ filteredTitle.textContent = "Pets[] filtered"
 filteredSection.textContent = 
 	`Only the M names: ${petsFiltered}!`
 
+// bare looping 
 
+let resultDiv = document.createElement("div")
+let sqRootSection = document.createElement("section")
+let sqRootTitle = document.createElement("span")
+let sqRootInput = document.createElement("input")
+let sqRootBtnCalc = document.createElement("button")
+sqRootBtnCalc.textContent = "Calculate"
+sqRootBtnCalc.addEventListener("click", function () {sqRoot(sqRootInput.value)})
+let sqRootBtnClear = document.createElement("button")
+sqRootBtnClear.textContent = "Clear"
+sqRootBtnClear.addEventListener("click", 
+	function () {resultDiv.textContent = " "; sqRootInput.value = " "})
+document.querySelector(".container").appendChild(sqRootSection)
+document.querySelector(".container").insertBefore(sqRootTitle, sqRootSection)
+sqRootTitle.textContent = "A simple loop"
+sqRootSection.textContent = "Enter a number and hit the button to calculate up to that value. "
+sqRootSection.appendChild(sqRootInput)
+sqRootSection.appendChild(sqRootBtnCalc)
+sqRootSection.appendChild(sqRootBtnClear)
+
+function sqRoot (limit) {
+	if (resultDiv.textContent != " "){ resultDiv.textContent = " " }
+	for (x = 1; x <= limit; x++) {
+		let result
+		result = x * x
+		// console.log(x + " x " + x + " = " + result)
+		resultDiv.innerHTML += x + " x " + x + " = " +  result + "<br>"
+		sqRootSection.appendChild(resultDiv)
+	}
+}
+
+// PAGE 2
+
+let testDiv = document.createElement("span")
+testDiv.textContent = "Hi."
+document.querySelector(".container").appendChild(testDiv)
