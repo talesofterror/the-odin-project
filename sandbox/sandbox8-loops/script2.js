b578349fb8054a7c271568e4df65db4cf3587466
mike <mich.rivera84@gmail.com>
2023-02-11 12:22:03 -0500

    sandbox8 loops, mdn exercises

--- sandbox/sandbox8-loops/script2.js
+++ sandbox/sandbox8-loops/script2.js
@@ -1,8 +1,41 @@
-let output = document.querySelector('.output');
-output.innerHTML = '';
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
-// let i = 10;
+let admitted = 'Admit: ';
+let refused = 'Refuse: ';
+let refusedArr = []
+let admitArr = []
 
-// const para = document.createElement('p');
-// para.textContent = ;
-// output.appendChild(para);
+for (i = 0; i < people.length; i++) {
+
+	if (people[i] == "Phil" || people[i] == "Lola") {
+		refusedArr.push(people[i])
+	}
+	else {
+		admitArr.push(people[i])
+	}
+}
+
+for (j = 0; j < refusedArr.length; j++){
+	if (j == refusedArr.length - 1) {
+		refused += `${refusedArr[j]}.`
+	} else {
+		refused += `${refusedArr[j]}, ` 
+	}
+}
+
+for (k = 0; k < admitArr.length; k++) {
+	if (k == admitArr.length - 1) {
+		admitted += `${admitArr[k]}.`
+	} else {
+		admitted += `${admitArr[k]}, `
+	}
+}
+
+// first iter: 
+// j = 0; refused.length = 2
+// if j == 0
+
+
+console.log("\n" + admitted + "\n" + refused)
+
+
