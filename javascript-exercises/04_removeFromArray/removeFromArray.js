cfdce708d8af4062ae3229cc7ce2cad312bde24d
mike <mich.rivera84@gmail.com>
2023-02-16 22:47:12 -0500

    javascript-exercises: removeFromArray

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -2,15 +2,37 @@ const removeFromArray = function(array, target) {
 	let newArr = []
 	let i_newArr = 0
 	for (i = 0; i <= array.length - 1; i++) {
-		if (array[i] == target) {
-			continue	
-		} else {
-			newArr[i_newArr] = array[i]
-			i_newArr++
+		for (j = 0; j <= arguments.length-1; j++) {
+			if (array[i] == arguments[j+1]) {
+				continue	
+			} else {
+					if (i_newArr[i] == array[i]) {
+						continue
+					} else {
+					newArr[i] = array[i]
+					i_newArr++
+					}
+			}
 		}
 	}
+	
 	return newArr
 };
+
+//const removeFromArray = function(array, target) {
+//	let newArr = []
+//	let i_newArr = 0
+//	for (i = 0; i <= array.length - 1; i++) {
+//		if (array[i] == target) {
+//			continue	
+//		} else {
+//			newArr[i_newArr] = array[i]
+//			i_newArr++
+//		}
+//	}
+//	return newArr
+//};
+
 
 
 // Do not edit below this line
--- sandbox/sandbox8-loops/script2.js
+++ sandbox/sandbox8-loops/script2.js
@@ -43,14 +43,89 @@ const removeFromArray = function(array, target) {
 	let newArr = []
 	let i_newArr = 0
 	for (i = 0; i <= array.length - 1; i++) {
-		if (array[i] == target) {
-			continue	
+		for (j = 0; j <= arguments.length-2; j++) {
+			if (newArr[i_newArr] == array[i]) {
+				continue
+			} else {
+				if (array[i] == arguments[j+1]) {
+					continue	
+				} else {
+					newArr[i_newArr] = array[i]
+					i_newArr++
+					}
+			}
+		}
+		
+	}
+	
+	return newArr
+};
+
+
+// maybe create an separate array from arguments[1] through arguments[n] and
+// search it separately 
+
+const removeFromArray2 = function(array, target) {
+	let newArr = []
+	let i_newArr = 0
+	for (i = 0; i <= array.length - 1; i++) {
+		for (j = 0; j < arguments.length-1; j++) {
+			if (array[i] == arguments[j+1]) {
+				continue	
+			} else {
+				if (newArr[i_newArr-1] == array[i]) {
+					continue
+				} else {
+					newArr[i_newArr] = array[i]
+					i_newArr++
+				}
+			}
+		}
+		
+	}
+	
+	return newArr
+};
+
+const removeFromArray3 = function (array) {
+	newArr = []
+	argArr = []
+	i_newArr = 0
+	for (i = 0; i <= arguments.length-2; i++) {
+		argArr[i] = arguments[i+1]	
+	}
+
+	for (i = 0; i <= array.length - 1; i++) {
+		if (array[i] == argArr[i]) {
+			continue
 		} else {
 			newArr[i_newArr] = array[i]
 			i_newArr++
 		}
 	}
-	return newArr
-};
 
+	return newArr;
+}
+
+const removeFromArray4 = function (array) {
+	newArr = []
+	argArr = []
+	i_newArr = 0
+	for (i = 0; i <= arguments.length-2; i++) {
+		argArr[i] = arguments[i+1]	
+	}
+
+	for (i = 0; i <= argArr.length-1; i++) {
+		for (j = 0; j <= array.length-1; j++) {	
+			if (argArr[i] == array[j]) {
+				continue
+			} else {
+				newArr[i_newArr] = array[j]
+				i_newArr++
+			}
+		}
+	}
+
+	return newArr;
+}
 
