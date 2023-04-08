6de937e0e7b15e03b24ea084ba21a5279d5d635c
mike <mich.rivera84@gmail.com>
2023-04-08 00:25:50 -0400

    javascript exercises

--- sandbox/sandbox8-loops/script2.js
+++ sandbox/sandbox8-loops/script2.js
@@ -281,3 +281,25 @@ const removeFromArray7 = function (array) {
 	}
 	return newArr;
 }
+
+
+const sumAll = function() {
+	let maxedVals = [arguments[0], arguments[1]].sort((x, y) => x-y)
+	let holsterArray = []
+	let answer = 0 
+
+	if (typeof arguments[0] != "number" || typeof arguments[1] != "number") return "ERROR"
+	if (arguments[0] < 0 || arguments[1] < 0) return "ERROR"
+
+	for (i = 0; i < maxedVals[1]; i++) {
+		holsterArray[i] = maxedVals[0] + i
+	}
+
+	for (i = 0; i <= holsterArray.length -1; i++) {
+		answer += holsterArray[i]
+	}
+	
+	return answer
+	
+};
+
