e4792b7d565fdbe0152637e223ab67c8b063d4be
mike <mich.rivera84@gmail.com>
2023-04-10 23:08:04 -0400

    leapYears solved

--- sandbox/sandbox8-loops/script2.js
+++ sandbox/sandbox8-loops/script2.js
@@ -303,3 +303,21 @@ const sumAll = function() {
 	
 };
 
+/*
+Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+*/
+
+const leapYears = function () {
+
+let inputFour = arguments[0] / 4
+let inputOneHundred = arguments[0] / 100
+let inputFourHundred = arguments[0] / 400
+if (inputFour == Math.floor(inputFour)) {
+	if(inputOneHundred == Math.floor(inputOneHundred)){
+			if (inputFourHundred == Math.floor(inputFourHundred)){
+			return true
+		} else return false
+	} else return true
+}
+else return false
+}
