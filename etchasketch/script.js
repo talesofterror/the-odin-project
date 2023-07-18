02936912c77aa379c18ce24974a339e5d61ee85a
mike <mich.rivera84@gmail.com>
2023-07-18 10:01:52 -0400

    created the etchasketch folder

---
+++ etchasketch/script.js
@@ -0,0 +1,24 @@
+let mainNodeList = document.querySelectorAll("main")
+let mainElement = document.querySelector("main")
+
+mainNodeList.forEach(element => console.dir(element))
+
+for (i = 0; i < 10; i++) {
+    mainElement.appendChild(document.createElement("div"))
+    let codePointString = `0x1F60${i}`
+    mainElement.lastElementChild.textContent = 
+        `${String.fromCodePoint(codePointString)}`
+    mainElement.lastElementChild.style.border = "1px solid red"
+}
+
+// CODE BELOW ONLY ADDS A SINGLE ELEMENT TO THE END OF THE CHILD LIST
+//
+// mainNodeList.forEach(element => 
+//     {
+//         element.appendChild(document.createElement("div"))
+//         mainElement.lastElementChild.textContent = `${String.fromCodePoint(0x1F600)}`
+//         mainElement.lastElementChild.style.border = "1px solid red"
+//     }
+// )
+
+mainElement.childNodes.forEach(element => console.log(element))
