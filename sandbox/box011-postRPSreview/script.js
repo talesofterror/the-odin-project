826c51b828fe293cc25ea7a014328fa60e85f6f4
mike <mich.rivera84@gmail.com>
2023-08-07 22:18:05 -0400

    want to look into factory model for organizing etchasketch grid

--- sandbox/box011-postRPSreview/script.js
+++ sandbox/box011-postRPSreview/script.js
@@ -3,15 +3,38 @@ let mainElement = document.querySelector("main")
 
 // mainNodeList.forEach(element => console.dir(element))
 
-for (x = 0; x < 10; x++) {
-    
-    for (y = 0; y < 10; y++){
+for (x = 0; x < 51; x++) {
+        
         mainElement.appendChild(document.createElement("div"))
         let codePointString = `0x1F60${x}`
-        mainElement.lastElementChild.textContent = 
-            `${String.fromCodePoint(codePointString)} ` + (x+1)
-    }
+        // mainElement.lastElementChild.textContent = 
+        //     //`${String.fromCodePoint(codePointString)} ` + (x+1)
+        //     x+1
 }
+
+mainElement.childNodes.forEach(element => console.log(element))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// GRAVEYARD
+
 
 // // CODE BELOW ONLY ADDS A SINGLE ELEMENT TO THE END OF THE CHILD LIST
    // ~ it was because the nodelist doesn't have any children to forEach
@@ -25,5 +48,3 @@ for (x = 0; x < 10; x++) {
 //         `${String.fromCodePoint(codePointString)} ` + (i+1)
 //     }
 // )
-
-mainElement.childNodes.forEach(element => console.log(element))
