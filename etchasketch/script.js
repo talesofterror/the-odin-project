ed7767cc393b02bb9d9f7117378230fb759d3e24
talesofterror <mich.rivera84@gmail.com>
2023-11-27 23:46:37 -0500

    thinking about twisting the knobs

--- etchasketch/script.js
+++ etchasketch/script.js
@@ -11,132 +11,159 @@ let initColor = getComputedStyle(document.documentElement).getPropertyValue("--d
 let screenCursor = getComputedStyle(document.documentElement).getPropertyValue("--screenCursor")
 
 let controls = {
-	color: {	
-		fgValue: fgColorPicker.value,
-		bgValue: bgColorPicker.value
-		//background color change should preserve drawing
-	},
-	resolution: { 
-		value: 50,
+  color: {
+    fgValue: fgColorPicker.value,
+    bgValue: bgColorPicker.value
+    //background color change should preserve drawing
+  },
+  resolution: {
+    value: 50,
     visible: true,
     elementToggleGrid: document.getElementById("grid-toggle"),
-		elementUp: document.getElementById("up-arrow"),
-		elementDown: document.getElementById("down-arrow"),
-		elementText: document.getElementById("resolution-text"),
-	},
-	drawing: false,
-	eraser: false,
-	randomize:{ 
-		element: document.querySelector(".randomize-toggle img"),
-		value: false },
-	clear: {
-	// maybe this should be called "clear"
-		element: document.getElementById("reset"),
-		method: function (bgColor) {
-			for (y = 0; y < pixels.length; y++) {
-				for (x = 0; x < pixels[y].length; x++){
-					colorCell(pixels[y][x].getAttribute("x"), pixels[y][x].getAttribute("y"), bgColor)
-				}}}
-	 }
+    elementUp: document.getElementById("up-arrow"),
+    elementDown: document.getElementById("down-arrow"),
+    elementText: document.getElementById("resolution-text"),
+  },
+  drawing: false,
+  eraser: false,
+  randomize: {
+    element: document.querySelector(".randomize-toggle img"),
+    value: false
+  },
+  clear: {
+    element: document.getElementById("reset"),
+    method: function (bgColor) {
+      for (y = 0; y < pixels.length; y++) {
+        for (x = 0; x < pixels[y].length; x++) {
+          colorCell(pixels[y][x].getAttribute("x"), pixels[y][x].getAttribute("y"), bgColor)
+        }
+      }
+    }
+  }
 }
-
 
 createScreen(controls.resolution.value)
 activateControls()
 
+function rotateKnobs () {
+  let knobs = [ document.querySelector(".left-knob"),
+                document.querySelector(".right-knob")]
+}
 
-function activateControls () {
+function activateControls() {
 
-	// randomize element
-	controls.randomize.element.addEventListener("mouseover", ()=> {
-		controls.randomize.element.classList.add("icon-color-mousein")
-		controls.randomize.element.classList.remove("icon-color-mouseout")
-	})
-	controls.randomize.element.addEventListener("mouseout", ()=> {
-		if (controls.randomize.value) { return }
-		else {
-		controls.randomize.element.classList.add("icon-color-mouseout")
-		controls.randomize.element.classList.remove("icon-color-mousein")
-		}
-	})
-	controls.randomize.element.addEventListener("click", ()=> {
-		if (!controls.randomize.value) {
-			controls.randomize.value = true 
-			controls.randomize.element.classList.add("icon-color-mousein")
-			controls.randomize.element.classList.remove("icon-color-mouseout")
-			controls.randomize.element.src = "assets/dice-color-icon.svg"
-			document.documentElement.style.setProperty("--screenCursor", makeRandomColorString())
-		} else { 
-			controls.randomize.value = false 
-			controls.randomize.element.classList.add("icon-color-mouseout")
-			controls.randomize.element.classList.remove("icon-color-mousein")
-			controls.randomize.element.src = "assets/dice-icon.svg"
-			document.documentElement.style.setProperty("--screenCursor", controls.color.fgValue)
-		}
-	})
-	// resolution elements
-	controls.resolution.elementUp.addEventListener("click", () => {
+  // randomize element
+  controls.randomize.element.addEventListener("mouseover", () => {
+    controls.randomize.element.classList.add("icon-color-mousein")
+    controls.randomize.element.classList.remove("icon-color-mouseout")
+  })
+  controls.randomize.element.addEventListener("mouseout", () => {
+    if (controls.randomize.value) { return }
+    else {
+      controls.randomize.element.classList.add("icon-color-mouseout")
+      controls.randomize.element.classList.remove("icon-color-mousein")
+    }
+  })
+  controls.randomize.element.addEventListener("click", () => {
+    if (!controls.randomize.value) {
+      controls.randomize.value = true
+      controls.randomize.element.classList.add("icon-color-mousein")
+      controls.randomize.element.classList.remove("icon-color-mouseout")
+      controls.randomize.element.src = "assets/dice-color-icon.svg"
+      document.documentElement.style.setProperty("--screenCursor", makeRandomColorString())
+    } else {
+      controls.randomize.value = false
+      controls.randomize.element.classList.add("icon-color-mouseout")
+      controls.randomize.element.classList.remove("icon-color-mousein")
+      controls.randomize.element.src = "assets/dice-icon.svg"
+      document.documentElement.style.setProperty("--screenCursor", controls.color.fgValue)
+    }
+  })
+  // resolution elements
+  controls.resolution.elementUp.addEventListener("click", () => {
     if (controls.resolution.value == 100) {
       return
     } else {
       controls.resolution.value += 10
       createScreen(controls.resolution.value)
-    } 
+    }
   })
-	controls.resolution.elementDown.addEventListener("click", () => {
+  controls.resolution.elementDown.addEventListener("click", () => {
     if (controls.resolution.value == 50) {
       return
     } else {
       controls.resolution.value -= 10
       createScreen(controls.resolution.value)
-    } 
+    }
   })
-	controls.resolution.elementToggleGrid.addEventListener("click", () => {
-		if (controls.resolution.visible){
-      for (y =0; y < pixels.length; y++){
-        for (x = 0; x < pixels[y].length; x++){
+  controls.resolution.elementToggleGrid.addEventListener("click", () => {
+    if (controls.resolution.visible) {
+      for (y = 0; y < pixels.length; y++) {
+        for (x = 0; x < pixels[y].length; x++) {
           pixels[y][x].style.borderWidth = "0px"
         }
       }
       controls.resolution.visible = false
     } else {
-      for (y =0; y < pixels.length; y++){
-        for (x = 0; x < pixels[y].length; x++){
+      for (y = 0; y < pixels.length; y++) {
+        for (x = 0; x < pixels[y].length; x++) {
           pixels[y][x].style.borderWidth = "1px"
         }
       }
       controls.resolution.visible = true
     }
-	})
-	// reset element
-	controls.clear.element.addEventListener("click", () => 
-		controls.clear.method(controls.color.bgValue))
+  })
+  // reset element
+  controls.clear.element.addEventListener("click", () =>
+    controls.clear.method(controls.color.bgValue))
+  // add color picker listeners
+  fgColorPicker.addEventListener("change", () => {
+    console.log(fgColorPicker.value)
+    controls.color.fgValue = fgColorPicker.value
+    document.documentElement.style.setProperty("--screenCursor", controls.color.fgValue)
+  })
+  bgColorPicker.addEventListener("change", () => {
+    console.log(bgColorPicker.value)
+    controls.color.bgValue = bgColorPicker.value
+    for (let y = 0; y < pixels.length; y++) {
+      for (let x = 0; x < pixels[y].length; x++) {
+        pixels[y][x].style.backgroundColor = controls.color.bgValue
+      }
+    }
+  })
+  // add knob reactivity
+  pixelContainer.addEventListener("mouseover", (e) => 
+    {
+      let pixelContainerCoords = pixelContainer.getBoundingClientRect()
+      console.log("mouse screen position = " + e.clientX + ", " + e.clientY)
+      console.log("pixel container coords = " + pixelContainerCoords.x + ", " + pixelContainerCoords.y)
+    })
 }
 
 function colorCell(x, y, color) {
-	pixels[y][x].style.backgroundColor = color
+  pixels[y][x].style.backgroundColor = color
 }
 
 function map(value, low1, high1, low2, high2) {
-	return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+  return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
 
-function map256To16 (color) { return map(color, 0, 15, 0, 255) }
+function map256To16(color) { return map(color, 0, 15, 0, 255) }
 
-function makeRandomColorValue () {
-	let colorVal = Math.floor(Math.random() * 255)
-	return colorVal
+function makeRandomColorValue() {
+  let colorVal = Math.floor(Math.random() * 255)
+  return colorVal
 }
-function makeRandomColorValue16 () {
-	let colorVal = Math.floor(Math.random() * 16)
-	return colorVal
+function makeRandomColorValue16() {
+  let colorVal = Math.floor(Math.random() * 16)
+  return colorVal
 }
 
-function makeRandomColorString () {
-	let r = map256To16(makeRandomColorValue16())
-	let g = map256To16(makeRandomColorValue16())
-	let b = map256To16(makeRandomColorValue16())
-	return `rgb(${r}, ${g}, ${b})`
+function makeRandomColorString() {
+  let r = map256To16(makeRandomColorValue16())
+  let g = map256To16(makeRandomColorValue16())
+  let b = map256To16(makeRandomColorValue16())
+  return `rgb(${r}, ${g}, ${b})`
 }
 
 function tooltipHover(event, divid) {
@@ -154,100 +181,85 @@ function tooltipOff(divid) {
 }
 
 function createScreen(sizeX) {
-	// Insert elements
-	if (pixelContainer.firstElementChild) {
-		Array.from(pixelContainer.children).forEach(element => element.remove())
-		pixels.length = 0
-		createScreen(sizeX)
-	} else {
-		for (let y = 0; y < sizeX / 2; y++) {
-			pixelContainer.appendChild(document.createElement("div"))
-			pixelContainer.lastChild.classList.add("pixel-row")
-			for (let x = 0; x < sizeX; x++) {
-				pixelContainer.lastChild.appendChild(document.createElement("div"))
-				pixelContainer.lastChild.lastChild.classList.add("pixel")
-				pixelContainer.lastChild.lastChild.setAttribute("x", x)
-				pixelContainer.lastChild.lastChild.setAttribute("y", y)
-			}
-		}
-	}
-	// Create pixel array
-	for (let i = 0; i < pixelContainer.children.length; i++) {
-		let p = []
-		for (let j = 0; j < pixelContainer.children[i].children.length; j++) {
-			pixelContainer.children[i].children[j].style.backgroundColor = controls.color.bgValue
+  // Insert elements
+  if (pixelContainer.firstElementChild) {
+    Array.from(pixelContainer.children).forEach(element => element.remove())
+    pixels.length = 0
+    createScreen(sizeX)
+  } else {
+    for (let y = 0; y < sizeX / 2; y++) {
+      pixelContainer.appendChild(document.createElement("div"))
+      pixelContainer.lastChild.classList.add("pixel-row")
+      for (let x = 0; x < sizeX; x++) {
+        pixelContainer.lastChild.appendChild(document.createElement("div"))
+        pixelContainer.lastChild.lastChild.classList.add("pixel")
+        pixelContainer.lastChild.lastChild.setAttribute("x", x)
+        pixelContainer.lastChild.lastChild.setAttribute("y", y)
+      }
+    }
+  }
+  // Create pixel array
+  for (let i = 0; i < pixelContainer.children.length; i++) {
+    let p = []
+    for (let j = 0; j < pixelContainer.children[i].children.length; j++) {
+      pixelContainer.children[i].children[j].style.backgroundColor = controls.color.bgValue
       p.push(pixelContainer.children[i].children[j])
-		}
-		pixels.push(p)
-	}
-	// Add pixel listeners
-	for (let y = 0; y < pixels.length; y++) {
-		for (let x = 0; x < pixels[y].length; x++) {
-			pixels[y][x].addEventListener("mousemove",
-				(e) => {
-					if (controls.drawing) {
-						e.preventDefault()
-						if (controls.eraser) {
-							colorCell(mousePosition[0], mousePosition[1], controls.color.bgValue)
-						} else if (controls.randomize.value) { 
-							colorCell(mousePosition[0], mousePosition[1], makeRandomColorString())
-						} else { colorCell(mousePosition[0], mousePosition[1], controls.color.fgValue)
-						}
-					}
-				})
-		}
-	}
-	// Add screen listeners
-	// ** if I click inside the pixel container and then drag the mouse
-	// ** out of the container and let go of the mouse colorcell() is
-	// ** still being called when I move my mouse back in the container
-	pixelContainer.addEventListener("mouseover",
-	(e) => {
-		if (controls.randomize.value) {
-			document.documentElement.style.setProperty("--screenCursor", makeRandomColorString())
-		}
-		mousePosition[0] = e.target.getAttribute("x")
-		mousePosition[1] = e.target.getAttribute("y")
-	})
-	pixelContainer.addEventListener("mousedown",
-		(e) => {
-			controls.drawing = true
-			console.log("drawing == " + controls.drawing)
-			console.log("button pressed == " + e.button)
-			console.log(e.target)
-			console.log("x = " + mousePosition[0] + "; y = " + mousePosition[1])
-			if (e.button == 0) { 
-				if (controls.randomize.value) {
-					screenCursor = () => makeRandomColorString()
-				}
-				controls.eraser = false
-				colorCell(mousePosition[0], mousePosition[1], controls.color.fgValue)
-			} else if (e.button == 2) {
-				controls.eraser = true
-				colorCell(mousePosition[0], mousePosition[1], controls.color.bgValue)
-				console.log("erasing")
-			}
-		})
-	document.querySelector("body").addEventListener("mouseup",
-		() => {
-			controls.drawing = false
-			console.log("drawing == " + controls.drawing)
-		})
-	fgColorPicker.addEventListener("change", () => {
-		console.log(fgColorPicker.value)
-		controls.color.fgValue = fgColorPicker.value
-		document.documentElement.style.setProperty("--screenCursor", controls.color.fgValue)
-
-	})
-	bgColorPicker.addEventListener("change", () => {
-		console.log(bgColorPicker.value)
-		controls.color.bgValue = bgColorPicker.value
-		for (let y = 0; y < pixels.length; y++) {
-			for (let x = 0; x < pixels[y].length; x++) {
-				pixels[y][x].style.backgroundColor = controls.color.bgValue
-			}
-		}
-	})
-	// resolution text value
-	controls.resolution.elementText.textContent = sizeX
+    }
+    pixels.push(p)
+  }
+  // Add screen listeners
+  // ** if I click inside the pixel container and then drag the mouse
+  // ** out of the container and let go of the mouse colorcell() is
+  // ** still being called when I move my mouse back in the container
+  for (let y = 0; y < pixels.length; y++) {
+    for (let x = 0; x < pixels[y].length; x++) {
+      pixels[y][x].addEventListener("mousemove",
+        (e) => {
+          if (controls.drawing) {
+            e.preventDefault()
+            if (controls.eraser) {
+              colorCell(mousePosition[0], mousePosition[1], controls.color.bgValue)
+            } else if (controls.randomize.value) {
+              colorCell(mousePosition[0], mousePosition[1], makeRandomColorString())
+            } else {
+              colorCell(mousePosition[0], mousePosition[1], controls.color.fgValue)
+            }
+          }
+        })
+    }
+  }
+  pixelContainer.addEventListener("mouseover",
+    (e) => {
+      if (controls.randomize.value) {
+        document.documentElement.style.setProperty("--screenCursor", makeRandomColorString())
+      }
+      mousePosition[0] = e.target.getAttribute("x")
+      mousePosition[1] = e.target.getAttribute("y")
+    })
+  pixelContainer.addEventListener("mousedown",
+    (e) => {
+      controls.drawing = true
+      console.log("drawing == " + controls.drawing)
+      console.log("button pressed == " + e.button)
+      console.log(e.target)
+      console.log("x = " + mousePosition[0] + "; y = " + mousePosition[1])
+      if (e.button == 0) {
+        if (controls.randomize.value) {
+          screenCursor = () => makeRandomColorString()
+        }
+        controls.eraser = false
+        colorCell(mousePosition[0], mousePosition[1], controls.color.fgValue)
+      } else if (e.button == 2) {
+        controls.eraser = true
+        colorCell(mousePosition[0], mousePosition[1], controls.color.bgValue)
+        console.log("erasing")
+      }
+    })
+  document.querySelector("body").addEventListener("mouseup",
+    () => {
+      controls.drawing = false
+      console.log("drawing == " + controls.drawing)
+    })
+  // resolution text value
+  controls.resolution.elementText.textContent = sizeX
 }
