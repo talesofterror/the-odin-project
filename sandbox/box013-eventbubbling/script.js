f8b5f394d493d9e499508355695b145c4062631e
mike <mich.rivera84@gmail.com>
2024-01-09 23:20:06 -0500

    sandbox 013

---
+++ sandbox/box013-eventbubbling/script.js
@@ -0,0 +1,10 @@
+let div1 = document.getElementById("div1")
+let div2 = document.getElementById("div2")
+
+div1.addEventListener("click", () => {
+  alert("div1 clicked!")
+})
+
+div2.addEventListener("click", () => {
+  alert("div2 clicked!")
+})
