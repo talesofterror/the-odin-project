424796acea72a8506ec6b6881ea9b743aff0df0e
mike <mich.rivera84@gmail.com>
2024-02-14 22:18:07 -0500

    palindromin

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+let split
+let backwardString = ''
+const palindromes = function (a) {
+  split = a.toLowerCase().split('')
+  let exclude = ["!", ".", ",", "?", / /g]
+  for (i = 0; i < split.length; i++){
+    if (split[i] == exclude.filter((e)=>e == split[i])){
+      split.splice(i, 1)
+    }
+  }
+  for (i = split.length-1; i >= 0; i--) {
+    backwardString += split[i]
+  }
+  return split.join('') == backwardString ? true : false
+}
 
 // Do not edit below this line
 module.exports = palindromes;
