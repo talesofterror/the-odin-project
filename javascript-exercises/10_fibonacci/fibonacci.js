3457d02a1b5d9876396c2e17a215cf6c8fad93b9
mike <mich.rivera84@gmail.com>
2024-02-15 21:36:03 -0500

    fibonaccing

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
+let prevValue = 0
+let value = 1
 const fibonacci = function(n) {
-  
+  for (let i = 1; i <= n; i++) { 
+    value += prevValue
+  }
+  return value
 };
+
+/*
+Fibonacci!
+
+need the Nth number of the fibonacci sequence
+
+
+
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
