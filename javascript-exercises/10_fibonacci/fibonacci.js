9b5004961152b8be558e564b14ca75f55ff465c5
mike <mich.rivera84@gmail.com>
2024-02-21 20:27:30 -0500

    fibonaccied

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,8 +1,26 @@
-const fibonacci = function(n) {
-  let zero = 0
-  let one = 1
-  for (let i = 1; i <= n; i++) { 
-    zero += one
+const fibonacci = function (n) {
+  if (n < 0){
+    return "OOPS"
+  }
+  backtrackArray = []
+  value = 1
+  prevValue = 0
+  for (let i = 0; i <= n; i++) {
+    if (i == 0) {
+      backtrackArray.push(0)
+      continue
+    }
+    if (i == 1) {
+      backtrackArray.push(1)
+      continue
+    }
+    value += prevValue
+    backtrackArray.push(value)
+    if (i < 1) {
+      prevValue = backtrackArray[i]
+    } else {
+      prevValue = backtrackArray[i - 1]
+    }
   }
   return value
 };
@@ -19,15 +37,15 @@ need the Nth number of the fibonacci sequence
 prevValue = 1
 i = 1 = N
 
-iter 0: 
+iter 0:
 
 1.  1
 2.  0 + 1 = 1
 3.  1 + 1 = 2
 4.  1 + 2 = 3
-5.  2 + 3 = 5 
+5.  2 + 3 = 5
 6.  3 + 5 = 8
-7.  5 + 8 = 13 
+7.  5 + 8 = 13
 8.  8 + 13 = 21
 9.  13 + 21 = 34
 10.  21 + 34 = 55
@@ -37,4 +55,4 @@ iter 0:
 */
 
 // Do not edit below this line
-//module.exports = fibonacci;
+module.exports = fibonacci;
