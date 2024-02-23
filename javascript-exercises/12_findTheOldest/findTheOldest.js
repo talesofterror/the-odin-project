c2cb421143c69a1cb604b5daf978cf7ecb8236eb
mike <mich.rivera84@gmail.com>
2024-02-23 12:19:23 -0500

    findtheoldest

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,28 +1,33 @@
 const findTheOldest = function(arr) {
-  let array
+  let array = []
+  arr.forEach((e) => {
+    if (!e.yearOfDeath){
+      let deathYear = new Date().getFullYear()
+      e.yearOfDeath = deathYear
+    }
+  })
   array = arr.toSorted( (a, b) => {
     return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
   })
   return array[0]
 };
 
-// const people = [
-//   {
-//     name: "Carly",
-//     yearOfBirth: 1942,
-//     yearOfDeath: 1970,
-//   },
-//   {
-//     name: "Ray",
-//     yearOfBirth: 1962,
-//     yearOfDeath: 2011,
-//   },
-//   {
-//     name: "Jane",
-//     yearOfBirth: 1912,
-//     yearOfDeath: 1941,
-//   },
-// ]
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
 // Do not edit below this line
 module.exports = findTheOldest;
