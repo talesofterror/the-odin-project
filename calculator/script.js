67c6a4e70418d11d915089fa11b9eab7587f3627
mike <mich.rivera84@gmail.com>
2024-02-29 22:43:03 -0500

    input: worked out key blocking and started on the button press reactivity functions

--- calculator/script.js
+++ calculator/script.js
@@ -1,17 +1,35 @@
+
+// numbers regex: ^[0-9]+$
+
 let input = document.getElementById("input")
 window.onload = function () {
   input.focus()
 }
+document.addEventListener("click", () => input.focus())
 
-function add (a, b) {
+let permittedChars = "1234567890+-*/.".split('')
+document.addEventListener("keydown", (e) => {
+  if (e.key == permittedChars.filter((el) => el == e.key)) {
+    console.log(`permitted character pressed: ${e.key}`)
+  }
+})
+
+input.addEventListener("input", (e) => {
+  if (e.data != permittedChars.filter((el) => el == e.data)) {
+    console.log(`key blocked: ${e.data}`)
+    input.value = input.value.slice(0, input.value.length-1)
+  }
+})
+
+function add(a, b) {
   return a + b
 }
-function subtract (a, b) {
+function subtract(a, b) {
   return a - b
 }
-function multiply (a, b) {
+function multiply(a, b) {
   return a * b
 }
-function divide (a, b) {
-  return a / b
+function divide(a, b) {
+  return b == 0 ? 0 : a / b
 }
