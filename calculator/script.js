9296e5732219f007ae4a078c3757759a7c0434de
mike <mich.rivera84@gmail.com>
2024-03-06 20:56:33 -0500

    added Operators object

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,5 @@
+let buttonColorDefault = getComputedStyle(document.documentElement).getPropertyValue("--buttonDefault")
+let buttonColorPressed = getComputedStyle(document.documentElement).getPropertyValue("--buttonPressed")
 
 let input = document.getElementById("input")
 window.onload = function () {
@@ -8,17 +10,47 @@ document.addEventListener("click", () => input.focus())
 let permittedChars = "1234567890+-*/.".split('')
 let buttonContainer = [].slice.call(document.getElementById("button-container").children)
 
+function Operators() {
+  this["+"] = function add(a, b) {
+    return a + b
+  }
+  this["-"] = function subtract(a, b) {
+    return a - b
+  }
+  this["*"] = function multiply(a, b) {
+    return a * b
+  }
+  this["/"] = function divide(a, b) {
+    return b == 0 ? 0 : a / b
+  }
+}
+
+let ops = new Operators()
+
+let equation = []
+
 document.addEventListener("keydown", (e) => {
   for (let i = 0; i < buttonContainer.length; i++) {
     if (e.key == buttonContainer[i].id.slice(5)){
-      buttonContainer[i].style.background = "#ff6633"
+      buttonContainer[i].style.background = buttonColorPressed
+      // & filter for permitted chars 9 to 14
+      // & clear input value and push to equation array
+      // & then wait for next number input
+      // & and either wait for another operator sign or equals
+      // & if (e.key == )
+    }
+    if (e.key == "Delete") {
+      buttonContainer[0].style.background = buttonColorPressed
+    }
+    if (e.key == "s") {
+      buttonContainer[1].style.background = buttonColorPressed
     }
   }
   console.log(e.key)
 })
 document.addEventListener("keyup", (e) => {
   for (let i = 0; i < buttonContainer.length; i++) {
-    buttonContainer[i].style.background = "#ffaa92"
+    buttonContainer[i].style.background = buttonColorDefault
   }
 })
 
@@ -29,16 +61,13 @@ input.addEventListener("input", (e) => {
   }
 })
 
+function operate (op) {
+  let opArr = op.split
+  return Operators[opArr[1]](opArr[0], opArr[2])
+}
 
-function add(a, b) {
-  return a + b
-}
-function subtract(a, b) {
-  return a - b
-}
-function multiply(a, b) {
-  return a * b
-}
-function divide(a, b) {
-  return b == 0 ? 0 : a / b
-}
+
+
+
+
+
