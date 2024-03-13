00d1325dd98890089f350efeaac9a3092833687e
mike <mich.rivera84@gmail.com>
2024-03-12 20:04:15 -0400

    working on the operation key problem

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,10 @@
+// & filter for permitted chars 9 to 14
+// & clear input value and push to equation array
+// & then wait for next number input
+// & and either wait for another operator sign or equals
+// & if (e.key == )
+
+
 let buttonColorDefault = getComputedStyle(document.documentElement).getPropertyValue("--buttonDefault")
 let buttonColorPressed = getComputedStyle(document.documentElement).getPropertyValue("--buttonPressed")
 
@@ -6,6 +13,19 @@ window.onload = function () {
   input.focus()
 }
 document.addEventListener("click", () => input.focus())
+
+input.addEventListener("input", (e) => {
+  if (permittedChars.slice(10, 14).includes(e.data)) {
+    console.log(`key blocked: ${e.data}`)
+    input.value = ""
+  }
+  if (e.data != permittedChars.filter((el) => el == e.data)
+    // || (e.data == permittedChars.slice(10, 14).filter(el => el == e.data) && input.value == "")
+    ) {
+    // console.log(`key blocked: ${e.data}`)
+    input.value = input.value.slice(0, input.value.length - 1)
+  }
+})
 
 let permittedChars = "1234567890+-*/.".split('')
 let buttonContainer = [].slice.call(document.getElementById("button-container").children)
@@ -30,14 +50,15 @@ let ops = new Operators()
 let equation = []
 
 document.addEventListener("keydown", (e) => {
+  if (permittedChars.slice(10, 14).includes(e.key)) {
+    console.log(`key blocked: ${e.key}`)
+    input.value = ""
+  }
+
   for (let i = 0; i < buttonContainer.length; i++) {
-    if (e.key == buttonContainer[i].id.slice(5)){
+    if (e.key == buttonContainer[i].id.slice(5)) {
       buttonContainer[i].style.background = buttonColorPressed
-      // & filter for permitted chars 9 to 14
-      // & clear input value and push to equation array
-      // & then wait for next number input
-      // & and either wait for another operator sign or equals
-      // & if (e.key == )
+
     }
     if (e.key == "Delete") {
       buttonContainer[0].style.background = buttonColorPressed
@@ -46,22 +67,18 @@ document.addEventListener("keydown", (e) => {
       buttonContainer[1].style.background = buttonColorPressed
     }
   }
-  console.log(e.key)
+  console.log("keydown: " + e.key)
 })
+
 document.addEventListener("keyup", (e) => {
   for (let i = 0; i < buttonContainer.length; i++) {
     buttonContainer[i].style.background = buttonColorDefault
   }
 })
 
-input.addEventListener("input", (e) => {
-  if (e.data != permittedChars.filter((el) => el == e.data)) {
-    // console.log(`key blocked: ${e.data}`)
-    input.value = input.value.slice(0, input.value.length-1)
-  }
-})
 
-function operate (op) {
+
+function operate(op) {
   let opArr = op.split
   return Operators[opArr[1]](opArr[0], opArr[2])
 }
