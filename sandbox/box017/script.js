4f86fbc92be8284d33c9a98f3dd5eafe45abc677
mike <mich.rivera84@gmail.com>
2024-06-22 15:26:04 -0400

    sandbox 017

--- sandbox/box017/script.js
+++ sandbox/box017/script.js
@@ -1,2 +1,8 @@
+const nameElement = document.getElementById("name")
+const submitElement = document.getElementById("submit")
+const resultsElement = document.getElementById("form-results")
 
+submitElement.addEventListener("click", ()=> {
+  resultsElement.textContent = nameElement.value
+})
 
