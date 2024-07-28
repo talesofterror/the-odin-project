ef93374dcfb0073d0c7d2e2327dba3c1f0ad9707
mike <mich.rivera84@gmail.com>
2024-07-28 00:47:54 -0400

    box026 - added scripted inputs

---
+++ sandbox/box026-regex/script.js
@@ -0,0 +1,30 @@
+let scriptedInputs = document.getElementsByClassName("scripted")
+// console.log(scriptedInputs)
+// console.log(typeof scriptedInputs)
+
+for (const e of scriptedInputs) {
+  e.classList.add("scripted-invalid")
+}
+
+document.addEventListener("input", (e) => {
+  if (e.target.classList[0] == "not-scripted") {
+    return
+  }
+  else if (e.target.value != "") {
+    let regexRule = new RegExp(e.target.getAttribute("data-pattern"))
+    let regExTest = regexRule.test(e.target.value)
+    if (regExTest == true) {
+      e.target.classList.remove("scripted-invalid")
+      e.target.classList.add("scripted-valid")
+    }
+    else {
+      e.target.classList.remove("scripted-valid")
+      e.target.classList.add("scripted-invalid")
+    }
+  }
+  else {
+    e.target.classList.remove("scripted-valid")
+    e.target.classList.add("scripted-invalid")
+  }
+})
+
