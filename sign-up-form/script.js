83abc6adf37fd6a7e598b0f32973e57c4ae2f346
mike <mich.rivera84@gmail.com>
2024-08-14 01:45:26 -0400

    sign up form password verification functions

--- sign-up-form/script.js
+++ sign-up-form/script.js
@@ -5,12 +5,13 @@
 let passwordInput = document.getElementById("password")
 let passwordConfirmInput = document.getElementById("password-confirm")
 
-document.addEventListener("change", (e) => {
+document.addEventListener("input", (e) => {
 	if (e.target == passwordInput) {
 		passwordInput.classList.add("pw-check")
+
 	}
 
-	if (e.target == passwordConfirmInput) {
+	if (e.target == passwordConfirmInput || e.target == passwordInput) {
 		if (passwordConfirmInput.value != passwordInput.value) {
 			passwordConfirmInput.classList.add("pw-confirm-check")	
 		} else {
