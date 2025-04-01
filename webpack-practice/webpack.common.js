3c77465bd7063cc6ae6b6f74836cd37221750eaf
mike <mich.rivera84@gmail.com>
2025-03-31 21:42:13 -0400

    webpack configs

--- webpack-practice/webpack.common.js
+++ webpack-practice/webpack.common.js
@@ -2,17 +2,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 
 module.exports = {
-	mode: "development",
 	entry: "./src/index.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
 		assetModuleFilename: "images/[name][ext]",
-	},
-	devtool: "eval-source-map",
-	devServer: {
-		watchFiles: ["./src/template.html"],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
