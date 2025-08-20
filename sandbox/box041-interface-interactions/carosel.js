af5638634263db1994297487ed194b32d107e408
mike <mich.rivera84@gmail.com>
2025-08-19 22:18:35 -0400

    box041 carosel

--- sandbox/box041-interface-interactions/carosel.js
+++ sandbox/box041-interface-interactions/carosel.js
@@ -1,3 +1,37 @@
+export let images = [
+	"./images/elmo1.jpg",
+	"./images/elmo2.jpg",
+	"./images/elmo3.jpg",
+	"./images/elmo4.jpg",
+]
+
+export function carosel_ImageElements (sourceArray) {
+	let array = []
+	for (let image of sourceArray) {
+		let imgElement = document.createElement("img")
+		let imgContainer = document.createElement("div")
+		imgElement.src = image
+		imgContainer.append(imgElement)
+		array.push(imgContainer)
+	}
+	return array
+}
+
+export function carosel_Dots (containerElement, displayElement, defaultClass, activeClass) {
+	let elementArray = carosel_ImageElements(images)
+
+	console.log(elementArray)
+	for (let image of elementArray) {
+		let dotElement = document.createElement("div")
+		dotElement.classList.add(defaultClass)
+		dotElement.addEventListener("click", ()=> {
+			displayElement.children[0].remove()
+			displayElement.append(image)
+		})
+		containerElement.append(dotElement)
+	}
+}
+
 export function carosel_Next () {
 
 }
--- sandbox/box041-interface-interactions/script.js
+++ sandbox/box041-interface-interactions/script.js
@@ -1,6 +1,8 @@
 import { dropdownEffectDisplay, dropdownEffectVisibility } from './dropdownmenu.js'
+import { carosel_Dots} from './carosel.js'
 
 dropdownEffectDisplay(document.getElementById("menu-button"), document.getElementById("menu-item-group"), "block")
+carosel_Dots(document.getElementById("carosel-dots"), document.getElementById("carosel-display"), "carosel-dot", "carosel-dot")
 
 
 
