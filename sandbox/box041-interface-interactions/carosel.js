2ab5217d5262eabb095fcd5248bac9f17b948986
mike <mich.rivera84@gmail.com>
2025-08-20 22:32:05 -0400

    box041 carosel

--- sandbox/box041-interface-interactions/carosel.js
+++ sandbox/box041-interface-interactions/carosel.js
@@ -1,42 +1,79 @@
-let images = [
-	"./images/elmo1.jpg",
-	"./images/elmo2.jpg",
-	"./images/elmo3.jpg",
-	"./images/elmo4.jpg",
-]
-
-export function carosel_ImageElements (sourceArray) {
-	let array = []
+// Create an carosel object
+export function carosel_Elements (sourceArray, displayElement, dotsContainer, defaultIndex = 0) {
+	let imageObject = {
+		sources: sourceArray,
+		imgElements: [],
+		displayElement: displayElement,
+		dotsContainer: dotsContainer,
+		dots: [],
+		activeIndex: defaultIndex
+	}
 	for (let image of sourceArray) {
 		let imgElement = document.createElement("img")
 		let imgContainer = document.createElement("div")
 		imgElement.src = image
 		imgContainer.append(imgElement)
-		array.push(imgContainer)
+		imageObject.imgElements.push(imgContainer)
 	}
-	return array
-}
-
-export function carosel_Dots (containerElement, displayElement, defaultClass, activeClass) {
-	let elementArray = carosel_ImageElements(images)
-
-	console.log(elementArray)
-	for (let image of elementArray) {
+	for (let element of imageObject.imgElements) {
 		let dotElement = document.createElement("div")
-		dotElement.classList.add(defaultClass)
-		dotElement.addEventListener("click", ()=> {
-			displayElement.children[0].remove()
-			displayElement.append(image)
+		imageObject.dots.push(dotElement)
+	}
+	return imageObject
+}
+
+// Initialize the carosel display
+// Populate dot container
+// Add dot styles
+// Prev/next functions
+export function carosel_Init (carosel, dotStyleDefault, dotStyleActive) {
+	let initObject = {
+		dotStyleActive: dotStyleActive,
+		dotStyleDefault: dotStyleDefault,
+		setDotsStyle: function () {
+			for (let i = 0; i < this.dots.length; i++) {
+				if (i == this.activeIndex) {
+					this.dots[i].classList.replace(dotStyleDefault, dotStyleActive)
+				} else {
+					this.dots[i].classList.replace(dotStyleActive, dotStyleDefault)
+				}
+			}
+		},
+		next: function () {
+			this.displayElement.children[0].remove()
+			if (this.activeIndex+1>this.imgElements.length-1) {
+				this.activeIndex = 0
+			} else {
+				this.activeIndex++
+			}
+			this.displayElement.append(this.imgElements[this.activeIndex])
+			this.setDotsStyle()
+		},
+		prev: function () {
+			this.displayElement.children[0].remove()
+			if (this.activeIndex-1<0) {
+				this.activeIndex = this.imgElements.length-1
+			} else {
+				this.activeIndex--
+			}
+			this.displayElement.append(this.imgElements[this.activeIndex])
+			this.setDotsStyle()
+		}
+	}
+	Object.assign(carosel, initObject)
+	carosel.displayElement.append(carosel.imgElements[carosel.activeIndex])
+	for (let dot = 0; dot < carosel.dots.length; dot++) {
+		carosel.dots[dot].classList.add(dotStyleDefault)
+		carosel.dots[dot].addEventListener("click", ()=> {
+			carosel.displayElement.children[0].remove()
+			carosel.displayElement.append(carosel.imgElements[dot])
+			carosel.activeIndex = dot
+			carosel.setDotsStyle()
 		})
-		containerElement.append(dotElement)
+		if (dot == carosel.activeIndex) { 
+			carosel.dots[dot].classList.replace(dotStyleDefault, dotStyleActive) 
+		}
+		carosel.dotsContainer.append(carosel.dots[dot])
 	}
 }
 
-export function carosel_Next (arrowElement, displayElement) {
-	let elementArray = carosel_ImageElements(images)
-	
-}
-
-export function carosel_Prev () {
-
-}
--- sandbox/box041-interface-interactions/script.js
+++ sandbox/box041-interface-interactions/script.js
@@ -1,8 +1,29 @@
 import { dropdownEffectDisplay, dropdownEffectVisibility } from './dropdownmenu.js'
-import { carosel_Dots} from './carosel.js'
+import { carosel_Init, carosel_Elements} from './carosel.js'
+
+let images = [
+	"./images/elmo1.jpg",
+	"./images/elmo2.jpg",
+	"./images/elmo3.jpg",
+	"./images/elmo4.jpg",
+]
+
+let carosel = carosel_Elements(images, document.getElementById("carosel-display"), document.getElementById("carosel-dots"))
+carosel_Init(carosel, "carosel-dot", "carosel-dot-active")
+
+document.getElementById("carosel-left").addEventListener("click", ()=>{
+	carosel.prev()
+})
+document.getElementById("carosel-right").addEventListener("click", ()=>{
+	carosel.next()
+})
+
+console.log(carosel.dotStyleActive)
+
 
 dropdownEffectDisplay(document.getElementById("menu-button"), document.getElementById("menu-item-group"), "block")
-carosel_Dots(document.getElementById("carosel-dots"), document.getElementById("carosel-display"), "carosel-dot", "carosel-dot")
+
+
 
 
 
