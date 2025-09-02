d3628e5214018f0d01afa850717c66871521b4b1
mike <mich.rivera84@gmail.com>
2025-09-01 23:29:47 -0400

    box043

--- sandbox/box043-working-with-api/script.js
+++ sandbox/box043-working-with-api/script.js
@@ -4,43 +4,55 @@ console.log("hi.")
 
 let weatherSection = document.getElementById("weather")
 let giphySection = document.getElementById("giphy")
+let giphySearchInput = document.getElementById("giphy-search-input")
+let giphySearchValue
+let giphySearchForm = document.getElementById("giphy-form")
 
-let giphyQuery = "chess"
-let giphyResponse
-let giphyRequest = `https://api.giphy.com/v1/gifs/search?`
+let giphyManualQuery = "chess"
+let giphyManualSearchRequest = `https://api.giphy.com/v1/gifs/search?`
 	+ `api_key=` + morsel 
-	+ `&q=` + giphyQuery 
+	+ `&q=` + giphyManualQuery 
 	+ `&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+let giphyInputSearchRequest = () => `https://api.giphy.com/v1/gifs/search?`
+	+ `api_key=` + morsel 
+	+ `&q=` + giphySearchValue
+	+ `&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+
 let giphyRefRequest = "./giphyRefData.json"
 
-let g_child01 = document.createElement("div")
+let giphyResponse
+
 let g_picture1 = document.getElementById("picture-1")
 let randomNum = () => Math.floor(Math.random() * 25)
 
+giphySearchForm.addEventListener( "submit", (e)=> {
+	e.preventDefault()
+	giphySearchValue = giphySearchInput.value
+	console.log(giphySearchInput.value)
+	giphyFetch(giphyInputSearchRequest())
+	console.log(giphyInputSearchRequest)
+} )
 
-fetch(giphyRefRequest)
+giphySearchInput.addEventListener( "change", () => {
+	giphySearchValue = giphySearchInput.value
+	console.log(giphySearchValue)
+})
+
+function giphyFetch (request) {
+	fetch(request)
 	.then( response => {
 		if (!response.ok) {
 			throw new Error("what the fuck: " + response.status)
 		}
 		giphyResponse = response.json
-		console.log(response)
 		return response.json()
 	})
 	.then( json => {
 		console.log(json)
+		console.log(giphySearchValue)
 		let img = document.createElement("img")
 		img.className = "gihpy-picture"
 		img.src = json.data[randomNum()].images.original.url
 		g_picture1.append(img)
 	})
-
-
-
-
-
-function giphyGrabber () {
-	
 }
-
-
