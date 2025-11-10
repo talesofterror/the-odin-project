5289689744e61b86a95997bba61a5b65b0135873
mike <mich.rivera84@gmail.com>
2025-11-10 00:10:58 -0500

    ...

--- sandbox/box045-recursion/script.js
+++ sandbox/box045-recursion/script.js
@@ -12,7 +12,7 @@ function pow_loop (x, n) {
 	return answer
 }
 
-console.log(pow_loop(5, 2)) // 3125
+console.log(pow_loop(5, 2)) // 25
 
 // writing a power function with recursion: 
 
@@ -23,7 +23,7 @@ function pow_recursive (x, n) {
 	return x * pow_recursive(x, n-1)
 }
 
-console.log(pow_recursive(5, 2))
+console.log(pow_recursive(5, 2)) // 25
 
 /*
 
@@ -45,3 +45,34 @@ console.log(pow_recursive(5, 2))
 // 																				// max depth depends on max call stack, but is 
 // 																				// set to an amount below the actually max call
 // 																				// stack size
+//
+// This is what the stack looks like:
+// pow(2, 4) = 2 * pow(2, 3)
+// pow(2, 3) = 2 * pow(2, 2)
+// pow(2, 2) = 2 * pow(2, 1)
+// pow(2, 1) = 2
+//
+// The maximal number of recursive calls is called recursion depth
+// Above it's four. It's equal to n in the function above.
+//
+// Each step above happens in the execution context. There's 4 separate execution contexts
+// in the example above. Contexts go into a special data structure called the execution
+// context stack. The that structure makes note of the value of each parameter and any variables declared up until the context was paused.
+//
+// For pow(2, 3): 
+// Context: { x: 2, n: 1, at line 1 } pow(2, 1)
+// Context: { x: 2, n: 2, at line 5 } pow(2, 2)
+// Context: { x: 2, n: 3, at line 5 }
+
+
+let initialValue = 0
+let array01 = [1, 1, 1, 1]
+console.log(
+
+	array01.reduce ( (accumulator, currentValue) => {
+		accumulator + currentValue
+	}, initialValue )
+
+)
+
+
