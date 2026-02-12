39bb029422264e24c57dcbe4e00dbea3e71511f3
mike <mich.rivera84@gmail.com>
2026-02-11 20:54:02 -0500

    recursion:totalIntegers -- passed

--- exercises/javascript-exercises/computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ exercises/javascript-exercises/computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,23 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(o) {
+	if (typeof o !== "object" || o === null) return
+
+	let values = Object.values(o)
+
+	let count = 0
+	for (let [i, value] of values.entries()) {
+		if (Number.isInteger(value)) {
+			count++
+		}
+		else {
+			if (value != null 
+				&& (value.__proto__ == {}.__proto__
+					|| value.__proto__ == [].__proto__)) {
+				count += totalIntegers(value)
+			}
+			else continue
+		}
+	}
+	return count 
 };
   
 // Do not edit below this line
--- sandbox/box045-recursion/script.js
+++ sandbox/box045-recursion/script.js
@@ -364,4 +364,28 @@ function contains6 (o, v) {
 
 console.log(contains6(object, 44))
 
+// ~~~~~~~~~~~~~~
+
+let integersArray = [1, 2, 3, "t", [3]]
+
+function totalIntegers (o) {
+	let values = Object.values(o)
+
+	let count = 0
+	for (let [i, value] of values.entries()) {
+		if (Number.isInteger(value)) {
+			count++
+		}
+		else {
+			if (value != null 
+				&& (value.__proto__ == {}.__proto__
+					|| value.__proto__ == [].__proto__)) {
+				count += totalIntegers(value)
+			}
+			else continue
+		}
+	}
+	return count
+}
+
 
