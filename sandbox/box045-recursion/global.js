32ceb5c08aac0f90a5f0833ab6707576ec4f05d4
mike <mich.rivera84@gmail.com>
2026-03-14 17:08:17 -0400

    recursion - fibonacci

--- sandbox/box045-recursion/global.js
+++ sandbox/box045-recursion/global.js
@@ -1,70 +1,62 @@
-// pascal
+// fibonacci (recursive)
 
-/*
-
-[0] 0 1 0	
-[1] 0 1 1 0
-[2] 0 1 2 1 0
-[3] 0 1 3 3 1 0
-[4] 0 1 4 6 4 1 0
-[5] 0 1 5 10 10 5 1 0
-
-*/
-
-function pascal (n, tracker = 0) {
-	let result
-	if (n <= 1) {
-		return [1]
+function fib (n, result = []) {
+	if (n <= 0) {
+		return [0 ,1]
 	}
 	else {
-		result = addRight(pascal(n-1), tracker++).filter( (n)=>n!=0 );
+		result.push(...fib(n-1))
+		result.push(result[n] + result[n-1])
+		return result
 	}
-	return result
 }
+// returns the correct array for all values except 
+// fib(0), which returns 
 
-function addRight (a) {
-  // [1, 2, 3] should == [1, 3, 5, 3]
-  // if (a.each( (i) => !Number.isInteger(i))) {return 0}
-	a.push(0)
-	a.splice(0, 0, 0)
-  let result = []
-  for (let [index, item] of a.entries()) {
-    if (index == 0) { result.push(item + 0); result.push(item + a[index+1])}
-    else if (index == a.length-1) { result.push(item + 0)}
-    else result.push( item + a[index+1])
-  }
-  return result
+function fib2 (n, result = [0]) {
+	if (n < 2) {
+		return [0]
+	}
+	else {
+		result.push( ...fib2(n-1), ...fib2(n-2) )
+		return result
+	}
 }
+// nope
 
-//permutations review
+function fibIndex (n) {
+	if (n<2) { return n }
+	else { 
+		return (fibIndex(n-1)+fibIndex(n-2))
+	}
+}
+// from the khan video -- returns the fibonacci number
+// at the n-index
 
-let permArray1 = [1, 2, 3, 4]; // js is forcing me to use a semicolon ;_;
-
-[permArray1[0], permArray1[2]] = [permArray1[2], permArray1[0]]
-
-
-function permute (array, index = 0, results = []) {
-  if (index == array.length) {
-    // We have formed a valid permutation.
-
-    // the [...array] syntax is a way to clone the contents of the array.
-    // because we do not want to pass a reference to the array, as that would mean
-    // that each item in `results` will be the same item
-    results.push([...array]);
-    return results;
-  }
-
-  for (let i = index; i < array.length; i++) {
-    // We use "destructuring assignment" here to swap the values of array[index] and array[i]
-    //
-    // More info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-    [array[index], array[i]] = [array[i], array[index]];
-    permute(array, index + 1, results);
-    [array[index], array[i]] = [array[i], array[index]];
-  }
-
-  return results;
-};
+function fib3 (n) {
+	if (n<2) { return n}
+	else { 
+		result = []
+		result.push(fib(n-1) + fib(n-2))
+		return result
+	}
+}
+// nope
 
 
-
+function fib4 (n, result = []) {
+	if (n <= 0) {
+		return [0]
+	}
+	if ( n == 1 ) {
+		return [0, 1]
+	}
+	else {
+		result.push(...fib(n-1))
+		result.push(result[n] + result[n-1])
+		return result
+	}
+}
+// works as far as returning the fibonacci sequence, 
+// but doesn't do it up to the right index: fib(5)
+// should return [0, 1, 1, 2, 3]
